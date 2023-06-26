aaef6731c728fbaa897a4c93339ec86872e0769b
Arturo Camacho <arturo.camacho.1dv@gmail.com>
2023-06-26 15:53:25 -0600

    Fix prefix api url

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'api'], function () {
+Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::resource('clients', ClientController::class);
 
     Route::resource('companies', CompanyController::class);
