a5d858a69af00b8facb3438539dc0b972c19bf49
Arturo Camacho <arturo.camacho.1dv@gmail.com>
2023-07-12 12:31:18 -0600

    Adding projects assigned route

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,8 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
 
     Route::resource('clients', ClientController::class);
     Route::post('clients/assign-project', [ClientController::class, 'assignProject']);
-    Route::get('/clients/projects/{projectId}', [ClientController::class, 'getAssignedClients']);
+    Route::get('clients/assigned-projects', [ClientController::class, 'getAssignedProjects']);
+    Route::get('clients/projects/{projectId}', [ClientController::class, 'getAssignedClients']);
 
     Route::resource('companies', CompanyController::class);
 
